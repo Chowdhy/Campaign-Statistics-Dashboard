3ac8f4b2ce38860be553685700cfa7dc27f36e65
Mahdi Chowdhury <mc5u22@soton.ac.uk>
2024-02-27 01:05:46 +0000

    Added SQLite dependencies, and fixed module-info file.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module uk.ac.soton.comp2211 {
+    requires java.scripting;
     requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires org.apache.logging.log4j;
+    requires org.xerial.sqlitejdbc;
     exports uk.ac.soton.comp2211;
 }
