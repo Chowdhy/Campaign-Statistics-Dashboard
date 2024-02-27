0e961a98ff5cda989af3332e866f1fa72c1c126a
Mahdi Chowdhury <mc5u22@soton.ac.uk>
2024-02-27 14:31:54 +0000

    Added OpenCSV dependency. (Also reordered dependencies)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,8 @@ module uk.ac.soton.comp2211 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-    requires org.apache.logging.log4j;
     requires org.xerial.sqlitejdbc;
+    requires com.opencsv;
+    requires org.apache.logging.log4j;
     exports uk.ac.soton.comp2211;
 }
