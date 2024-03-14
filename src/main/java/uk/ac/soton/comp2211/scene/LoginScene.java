472f67ece1f64cfcbbe05b72d749fd9abd22bf11
sebas <sc11g22@soton.ac.uk>
2024-03-14 18:47:48 +0000

    Create LoginScene

---
+++ src/main/java/uk/ac/soton/comp2211/scene/LoginScene.java
@@ -0,0 +1,24 @@
+package uk.ac.soton.comp2211.scene;
+
+import uk.ac.soton.comp2211.ui.MainWindow;
+
+public class LoginScene extends BaseScene {
+
+    public LoginScene(MainWindow window){
+        super(window);
+    }
+
+    @Override
+    public void initialise() {
+
+    }
+
+    @Override
+    public void build(){
+
+    }
+
+    @Override
+    public void cleanup(){
+    }
+}
