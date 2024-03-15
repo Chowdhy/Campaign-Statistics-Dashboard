e294395e19b8c863e9c42c9c7670e4392c6bb0d1
sebas <sc11g22@soton.ac.uk>
2024-03-15 03:50:59 +0000

    Updated looks and added dynamic selection to user modification check boxes

--- src/main/java/uk/ac/soton/comp2211/scene/ModifyUserScene.java
+++ src/main/java/uk/ac/soton/comp2211/scene/ModifyUserScene.java
@@ -2,15 +2,16 @@ package uk.ac.soton.comp2211.scene;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import uk.ac.soton.comp2211.ui.MainWindow;
 
 public class ModifyUserScene extends BaseScene{
+    private CheckBox adminCheckBox;
+    private CheckBox readWriteCheckBox;
+    private CheckBox viewerCheckBox;
 
     public ModifyUserScene(MainWindow window){
         super(window);
@@ -25,12 +26,12 @@ public class ModifyUserScene extends BaseScene{
         root = new StackPane();
         root.setFocusTraversable(false);
         var loginFields = new VBox();
-        var loginLabel = new Label("Ad Dashboard Login");
+        var loginLabel = new Label("Modify User");
         var usernameInput = new TextField();
-        usernameInput.setPromptText("Username");;
-        var loginButton = new Button("Login");
+        usernameInput.setPromptText("Username");
+        var loginButton = new Button("Modify");
         root.getChildren().add(loginFields);
-        loginFields.getChildren().addAll(loginLabel,usernameInput,loginButton);
+
         root.setPadding(new Insets(200,200,200,200));
         loginFields.setSpacing(10);
         loginFields.setAlignment(Pos.CENTER);
@@ -38,10 +39,51 @@ public class ModifyUserScene extends BaseScene{
         usernameInput.setFocusTraversable(false);
         loginButton.setFocusTraversable(false);
 
+        var adminHBox = new HBox();
+        var adminLabel = new Label("Admin");
+        var adminCheckBox = new CheckBox();
+        adminHBox.getChildren().addAll(adminCheckBox,adminLabel);
+        adminHBox.setSpacing(5);
+
+        var readWriteHBox = new HBox();
+        var readWriteLabel = new Label("Read & Write");
+        var readWriteCheckBox = new CheckBox();
+        readWriteHBox.getChildren().addAll(readWriteCheckBox,readWriteLabel);
+        readWriteHBox.setSpacing(5);
+
+        var viewerHBox = new HBox();
+        var viewerLabel = new Label("Viewer");
+        var viewerCheckBox = new CheckBox();
+        viewerHBox.getChildren().addAll(viewerCheckBox,viewerLabel);
+        viewerHBox.setSpacing(5);
+
+        loginFields.getChildren().addAll(loginLabel,adminHBox,readWriteHBox,viewerHBox,usernameInput,loginButton);
+
+
+        adminCheckBox.setOnAction(e -> {
+            if (adminCheckBox.isSelected()) {
+                readWriteCheckBox.setSelected(true);
+                viewerCheckBox.setSelected(true);
+            }
+        });
+
+
+        readWriteCheckBox.setOnAction(e -> {
+            if (readWriteCheckBox.isSelected()) {
+                viewerCheckBox.setSelected(true);
+            }
+        });
+
+
+        viewerCheckBox.setOnAction(e -> {
+            if (!viewerCheckBox.isSelected()) {
+                readWriteCheckBox.setSelected(false);
+                adminCheckBox.setSelected(false);
+            }
+        });
 
         loginButton.setOnAction(e ->{
             window.loadDashboard();
-            System.out.println("Done");
         });
     }
 
