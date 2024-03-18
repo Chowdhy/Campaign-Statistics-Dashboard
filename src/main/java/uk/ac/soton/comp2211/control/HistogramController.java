a9bf99fe47db7470caeb4ea4f4cd7d01423d90be
Hasan <ho2u21@soton.ac.uk>
2024-03-18 18:07:30 +0000

    Histogram added. UI changes based on user permission level. Back buttons added on multiple pages

---
+++ src/main/java/uk/ac/soton/comp2211/control/HistogramController.java
@@ -0,0 +1,54 @@
+package uk.ac.soton.comp2211.control;
+
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import uk.ac.soton.comp2211.data.graph.GraphData;
+import java.util.ArrayList;
+
+public class HistogramController {
+
+    GraphData graphData;
+
+    public HistogramController() {
+        this.graphData = new GraphData();
+    }
+
+
+    public void histogram(BarChart barChart) {
+        barChart.getData().clear();
+        ArrayList<Double> data = graphData.getClickCosts();
+
+        double minValue = Double.MAX_VALUE;
+        double maxValue = Double.MIN_VALUE;
+        for (Double value : data) {
+            minValue = Math.min(minValue, value);
+            maxValue = Math.max(maxValue, value);
+        }
+
+        int numBins = 10;
+        double binWidth = (maxValue - minValue) / numBins;
+
+
+        // Populate histogram with data
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        for (int i = 0; i < numBins; i++) {
+            double binStart = minValue + i * binWidth;
+            double binEnd = binStart + binWidth;
+            int frequency = countValuesInBin(data, binStart, binEnd);
+            String binLabel = String.format("%.2f - %.2f", binStart, binEnd);
+            series.getData().add(new XYChart.Data<>(binLabel, frequency));
+        }
+
+        barChart.getData().add(series);
+    }
+
+    private int countValuesInBin(ArrayList<Double> data, double binStart, double binEnd) {
+        int count = 0;
+        for (Double value : data) {
+            if (value >= binStart && value < binEnd) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/main/java/uk/ac/soton/comp2211/scene/HistogramScene.java
@@ -0,0 +1,57 @@
+package uk.ac.soton.comp2211.scene;
+
+import javafx.geometry.Pos;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import uk.ac.soton.comp2211.control.HistogramController;
+import uk.ac.soton.comp2211.ui.MainWindow;
+
+
+
+public class HistogramScene extends BaseScene {
+    BarChart<String, Number> histogram;
+    HistogramController hst = new HistogramController();
+
+    public HistogramScene(MainWindow window){
+        super(window);
+    }
+
+    @Override
+    public void initialise() {
+
+
+    }
+
+    @Override
+    public void build() {
+        root = new VBox();
+        StackPane stp = new StackPane();
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+        histogram = new BarChart<>(xAxis, yAxis);
+        hst.histogram(histogram);
+        stp.getChildren().add(histogram);
+        stp.centerShapeProperty();
+        root.getChildren().add(stp);
+        var buttons = new HBox();
+        var back = new Button("Back");
+        back.setOnAction( e -> window.loadDashboard());
+        buttons.getChildren().add(back);
+        root.getChildren().add(buttons);
+        buttons.setAlignment(Pos.CENTER);
+
+        histogram.setTitle("Histogram of Click costs(Cost Distribution)");
+        xAxis.setLabel("Ranges of click costs");
+        yAxis.setLabel("Frequency");
+
+    }
+
+
+    @Override
+    public void cleanup() {
+
+    }
+}
